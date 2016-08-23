b6c95904573f7893df7d237fd5fb0bebd4f0f6d7
Miguel Fagundez <mfagundez@mahisoft.com>
2016-08-23 09:58:15 -0400

    Adds two more tutorials

---
+++ uploading-files/src/main/java/hello/Application.java
@@ -0,0 +1,28 @@
+package hello;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.util.FileSystemUtils;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init() {
+        return (args) -> {
+            FileSystemUtils.deleteRecursively(new File(FileUploadController.ROOT));
+
+            Files.createDirectory(Paths.get(FileUploadController.ROOT));
+        };
+    }
+}
